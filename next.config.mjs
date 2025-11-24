eafc42a5093dc836b8c3af913f633fcdc9421b42
Rojal604 <maharjanrojal52@gmail.com>
2025-11-24 22:50:27 +0545

    Add assetPrefix to fix image paths on GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   basePath: isProd ? '/project-5' : '',
+  assetPrefix: isProd ? '/project-5' : '',
   images: {
     unoptimized: true,
   },
